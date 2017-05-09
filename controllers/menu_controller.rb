6d2f802609ad098c4b6aad736e41521729725122
Vincent Tsang <vstsang@gmail.com>
2017-05-09 21:37:34 +0800

    Assignment 8 Menu: completed

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -12,8 +12,9 @@ class MenuController
     puts "1 - View all entries"
     puts "2 - Create an entry"
     puts "3 - Search for an entry"
-    puts "4 - Import entries from a CSV"
-    puts "5 - Exit"
+    puts "4 - Delete all entries"
+    puts "5 - Import entries from a CSV"
+    puts "6 - Exit"
     print "Enter your selection: "
 
     selection = gets.to_i
@@ -32,9 +33,13 @@ class MenuController
         main_menu
       when 4
         system "clear"
-        read_csv
+        nuke
         main_menu
       when 5
+        system "clear"
+        read_csv
+        main_menu
+      when 6
         puts "Good-bye!"
         exit(0)
       else
@@ -179,6 +184,11 @@ class MenuController
         puts entry.to_s
         search_submenu(entry)
     end
+  end
+
+  def nuke
+    address_book.entries.clear
+    puts "All entries nuked!!"
   end
 
 end
